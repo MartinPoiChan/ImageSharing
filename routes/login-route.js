30fed1b60461b52479f7b28006175b71d9a6b138
MartinPoiChan <martinpoichan@gmail.com>
2021-11-03 21:37:20 +0200

    login and logout

---
+++ routes/login-route.js
@@ -0,0 +1,35 @@
+const express = require('express'),
+      app = express(),
+      pug     = require('pug'),
+      path    = require('path'),
+      session    = require("express-session"),
+      {buildParams} = require("../functions/buildparams"),
+      {getlog} = require("../controllers/login-controller"),
+      flash = require('connect-flash');
+
+app.set('views', path.join(__dirname, '../view/pages'));
+app.set('view engine', 'pug');
+app.use(express.static("assets"));
+// app.use(require('connect-flash')());
+app.get('/login', async(req, res) => {
+    res.render('login',buildParams(req, {page:'login'}))
+});
+
+app.post('/login', async(req, res) => {
+    let test = await getlog(req.body.UserID, req.body.UserPass);
+    console.log(test.code);
+    if(test.code == 200){
+        req.session.loggedin = true
+        req.session.fname = test.result[0].fname;
+        req.session.lname = test.result[0].lname;
+        req.session.uid = test.result[0].user_id;
+        req.flash('success', test.message);
+        res.redirect('/about');
+    }
+    else{
+        req.flash('error', test.message);
+        res.redirect('/login');
+    }
+});
+
+module.exports = app;
